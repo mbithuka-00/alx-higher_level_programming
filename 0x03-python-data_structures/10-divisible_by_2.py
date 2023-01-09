e300af174b6e221dd8758a00aa342f65522401c2
mbithuka-00 <mbithuka-00>
2023-01-08 23:11:01 -0800

    10-divisible_by_2.py commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
-
-def print_list_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+    new_list = []
     for i in my_list:
-        print('{:d}'.format(i))
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
