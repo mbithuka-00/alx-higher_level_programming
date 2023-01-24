60255f0c9f9bbe8117f2c2b4b14a1be25812816e
mbithuka-00 <mbithuka-00>
2023-01-24 06:56:28 -0800

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# By  MATHEW ALX SCHOOL
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
+
