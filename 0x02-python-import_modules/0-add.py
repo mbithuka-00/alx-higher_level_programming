f0dd938e50fd94fad5d5e0689061218f1574a820
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-11 06:51:53 -0700

    my commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+# author: mathew mbithuka
 
 if __name__ == "__main__":
-    '''print the addition of two numbers'''
+    """Print the sum of both numbers."""
     from add_0 import add
 
-    a=1
-    b=2
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
+
