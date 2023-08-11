93d33a92be97bb8a2b9d448654be9adc5032f94a
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-11 06:14:32 -0700

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == " __main__ ":
-
-    """print the number of lists and arguements in this code"""
+if __name__ == "__main__":
+    """Print the number of and list of arguments in this task."""
     import sys
 
     count = len(sys.argv) - 1
@@ -14,5 +13,4 @@ if __name__ == " __main__ ":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
 
