7ba611a7f72aefcadffe92745463c508c44e6cb4
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-11 02:01:36 -0700

    my commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a,b):
+    return a+b
+
+if __name__ == "__main__":
+    a=1
+    b=2
+    result = add(a ,b)
+    print(f"{a} + {b} = {result}\n")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+this is my projects for impoert and modules
