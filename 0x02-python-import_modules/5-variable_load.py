6ed60cad323063c703e455ec797a02eb52fc91a9
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-16 06:27:24 -0700

    my commit

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-#author: Mathew Mbithuka
+# author: Mathew Mbithuka
 
 if __name__ == "__main__":
     """Print the value of variable a from variable load 5."""
     from variable_load_5 import a
 
     print(a)
-
-
