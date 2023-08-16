66c28d79728f4f534b6f3e28ad69faf40c12433f
mbithuka-00 <mbithukamatthew@gmail.com>
2023-08-16 06:37:23 -0700

    my commit

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# author matt mbithuka
+
+
+def magic_calculation(a, b):
+    """Match bytecode """
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+        return (c)
+
+    else:
+        return(sub(a, b))
