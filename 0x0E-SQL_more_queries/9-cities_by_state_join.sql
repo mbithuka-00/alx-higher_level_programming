23230d19c606eccda59e0b78247da8b9d011e1f0
mbithuka-00 <mbithukamatthew@gmail.com>
2023-10-18 04:49:07 -0700

    my commit

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- this lists all cities contained in the database hbtn_0d_usa
+-- and then lists all rows of a particular column in a database
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
