e50744b78680e1167a2838299606eb259290c78c
mbithuka-00 <mbithukamatthew@gmail.com>
2023-12-01 02:11:16 -0800

    my commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    requ = Request(url)
+
+    with urlopen(requ) as response:
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,2 @@
+#this is the README file for the oython task 0x11-oython-network_1
+# in this project we are tasked on how to retrieve information from web pages using python using the library urllib
